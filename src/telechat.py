c314498ea830d6f822c49f2c3d68260b8943f88f
Johannes Bräuer <yanko397@web.de>
2023-08-25 23:45:35 +0200

    set temperature per command

--- src/telechat.py
+++ src/telechat.py
@@ -16,24 +16,27 @@ os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #     level=logging.INFO
 # )
 
-cookie_path_dir = 'cookies'
-logfile = 'log.txt'
+COOKIE_PATH_DIR = 'cookies'
+LOGFILE = 'log.txt'
+MAX_TRIES = 5
+
 chatbot = None
+temperature = 0.9
 
 
 def login():
-    os.makedirs(cookie_path_dir, exist_ok=True)
-    cookies_files = os.listdir(cookie_path_dir)
+    os.makedirs(COOKIE_PATH_DIR, exist_ok=True)
+    cookies_files = os.listdir(COOKIE_PATH_DIR)
     if cookies_files:
         mail = cookies_files[0][:-5] if len(cookies_files) == 1 else input('Mail: ')
         sign = Login(mail, None)
-        cookies = sign.loadCookiesFromDir(cookie_path_dir)
+        cookies = sign.loadCookiesFromDir(COOKIE_PATH_DIR)
     else:
         mail = input('Mail: ')
         pw = input('Password: ')
         sign = Login(mail, pw)
         cookies = sign.login()
-        sign.saveCookiesToDir(cookie_path_dir)
+        sign.saveCookiesToDir(COOKIE_PATH_DIR)
     return cookies.get_dict()
 
 
@@ -49,12 +52,12 @@ def new_chatbot():
 
 def log(sender, message):
     # TODO this is not thread safe
-    with open(logfile, 'a', encoding='utf-8') as f:
+    with open(LOGFILE, 'a', encoding='utf-8') as f:
         f.write(f'================{sender}================\n{message}\n')
 
 
-async def hugstart(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi I'm hugchat :) write anything\n\nRight now you still need to be whitelisted to get a response though. I'll change that soon!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"[bot]\nHi I'm hugchat :) write anything\n\nRight now you still need to be whitelisted to get a response though. I'll change that soon!\n\nCurrent temperature is {temperature}]\n\nUpdate with: /temperature [temperature]")
 
 
 async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -64,38 +67,54 @@ async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     if not update.message.text:
         return
-    tries_remaining = 3
+    tries_remaining = MAX_TRIES
     message = ''
     while not message and tries_remaining:
-        # print(f'trying to answer with {tries_remaining} tries left..')
         try:
-            message = chatbot.chat(update.message.text, temperature=0.5)
+            message = chatbot.chat(update.message.text, temperature=temperature)
         except Exception as e:
             print(e)
             tries_remaining -= 1
     if not message and not tries_remaining:
-        message = '[Nur gibberish als Antwort auch nach 3 Versuchen.. Sorry :( Kannst es aber gerne nochmal versuchen]'
+        message = f'[bot]\nNur gibberish als Antwort auch nach {MAX_TRIES} Versuchen.. Sorry :( Kannst es aber gerne nochmal versuchen'
     log(f'to {update.effective_user.username}', message)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_to_message_id=update.message.message_id)
 
 
+async def set_temperature(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global temperature
+    # no temperature given
+    if not context.args:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'[bot]\nCurrent temperature is {temperature}\n\nUpdate with: /temperature [temperature]')
+        return
+    # invalid temperature
+    if not context.args[0].replace('.', '', 1).isdigit() or not 0 < float(context.args[0]) <= 1:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'[bot]\nInvalid temperature: {context.args[0]}')
+        return
+    temperature = float(context.args[0])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'[bot]\nTemperature set to {temperature}')
+
+
 def main():
     global chatbot
+
+    # HuggingChat
     chatbot = new_chatbot()
 
-    print('init telegram..')
+    # Telegram
     config = loader.load_telegram_config()
     app = ApplicationBuilder().token(config['telegram_api_token']).build()
-    print('telegram ready!')
 
-    print('adding handlers..')
-    start_handler = CommandHandler('start', hugstart)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), answer)
+    # Telegram Handlers
+    start_handler = CommandHandler('start', start)
+    temperature_handler = CommandHandler('temperature', set_temperature)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), answer)
 
     app.add_handler(start_handler)
-    app.add_handler(echo_handler)
-    print('handlers ready!')
+    app.add_handler(temperature_handler)
+    app.add_handler(message_handler)
 
+    # Run
     print('starting polling..')
     app.run_polling()
 
